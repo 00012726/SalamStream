fcc6c28f0de8f27841a80bc3ea6495918c28bc71
00012726 <151643831+00012726@users.noreply.github.com>
2024-01-09 13:17:59 +0500

    added upload button commit

--- JS/app.js
+++ JS/app.js
@@ -7,3 +7,51 @@ menuIcon.onclick = function()
     sidebar.classList.toggle("smallSidebar");
     container.classList.toggle("largeContainer");
 }
+
+function showPopup() 
+{
+    var popupBackground = document.getElementById("popupBackground");
+    var popup = document.getElementById("popup");
+
+    popupBackground.style.display = "block";
+    popup.style.display = "block";
+}
+
+function hidePopup() 
+{
+    var popupBackground = document.getElementById("popupBackground");
+    var popup = document.getElementById("popup");
+
+    popupBackground.style.display = "none";
+    popup.style.display = "none";
+}
+
+
+
+// <------------INPUT TYPE FOR UPLOADING FILES----------------->
+function showFileInput(type) {
+    var fileInput = document.createElement("input");
+    fileInput.type = "file";
+
+    if (type === "video") 
+    {
+      fileInput.accept = "video/*";
+    } 
+    else if (type === "audio") 
+    {
+      fileInput.accept = "audio/*";
+    } 
+    else if (type === "book") 
+    {
+      fileInput.accept = ".pdf";
+    }
+
+    fileInput.addEventListener("change", function() 
+    {
+      // Handle file upload logic here
+      alert("File uploaded successfully!");
+      hidePopup();
+    });
+
+    fileInput.click();
+}
